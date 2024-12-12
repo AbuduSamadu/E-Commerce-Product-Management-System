c01fc58fe06a600fb2c2ed740d231965290cccd4
Abudu Samadu <abudu.samadu@amalitech.com>
2024-12-12 01:09:20 +0000

    Fix return type in AuditLogController's getAuditLogs method to return a list of audit logs

---
+++ src/main/java/abudu/product/controllers/AuditLogController.java
@@ -0,0 +1,33 @@
+package abudu.product.controllers;
+
+import abudu.product.models.AuditLog;
+import abudu.product.services.AuditLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/audit-log")
+public class AuditLogController {
+    private final AuditLogService auditLogService;
+
+    @Autowired
+    public AuditLogController(AuditLogService auditLogService) {
+        this.auditLogService = auditLogService;
+    }
+    @RequestMapping("/create")
+    public ResponseEntity<AuditLog> createAuditLog(String action, String details, String performedBy) {
+        AuditLog auditLog = auditLogService.createAuditLog(action, details, performedBy);
+        return ResponseEntity.ok(auditLog);
+    }
+
+    @RequestMapping("/get")
+    public ResponseEntity <List<AuditLog>> getAuditLogs() {
+        List<AuditLog> auditLog = auditLogService.getAuditLogs();
+        return ResponseEntity.ok(auditLog);
+    }
+
+}
---
+++ src/main/java/abudu/product/services/AuditLogService.java
@@ -0,0 +1,34 @@
+package abudu.product.services;
+
+
+import abudu.product.models.AuditLog;
+import abudu.product.repositories.AuditLogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class AuditLogService {
+    private final AuditLogRepository auditLogRepository;
+
+    @Autowired
+    public AuditLogService(AuditLogRepository auditLogRepository) {
+        this.auditLogRepository = auditLogRepository;
+    }
+
+    public AuditLog createAuditLog(String action, String details, String performedBy) {
+        AuditLog auditLog = new AuditLog();
+        auditLog.setAction(action);
+        auditLog.setDetails(details);
+        auditLog.setPerformedBy(performedBy);
+        auditLog.setTimestamp(LocalDateTime.now());;
+        return auditLogRepository.save(auditLog);
+    }
+
+    public List<AuditLog> getAuditLogs() {
+        return auditLogRepository.findAll();
+    }
+
+}
